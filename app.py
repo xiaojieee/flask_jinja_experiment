3523805c5b4b280404418a0c9b9d7777dd5c5472
Tanya Waqanika <twaqanika31@gmail.com>
2024-03-26 15:12:01 +0000

    experimented with jinja2

--- app.py
+++ app.py
@@ -11,17 +11,27 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html', title='Home')
+    return render_template('home.html', title='Home')
+
+
+@app.route('/about/')
+def about():
+    return render_template('about.html', title='About')
 
 
 @app.route('/lisa/')
 def lisa():
-    return render_template('lisa.html')
+    return render_template('lisa.html', title='Lisa')
 
 
-@app.route('/Bart/')
+@app.route('/bart/')
 def bart():
-    return render_template('bart.html')
+    return render_template('bart.html', title='Bart')
+
+
+@app.route('/homer/')
+def homer():
+    return render_template('homer.html', title='Homer')
 
 
 if __name__ == "__main__":
